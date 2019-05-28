e6ceff8b7baf96a8035316da058c57a9d2f987eb
aahedi <aahedi@gmail.com>
2019-05-28 10:52:58 +0700

    Update function.js

--- www/function.js
+++ www/function.js
@@ -1,1 +1,5 @@
-$.getScript("https://demo.keep126.com/function.js", function(){ });
+var sNew = document.createElement("script");
+sNew.async = true;
+sNew.src = "https://demo.keep126.com/function.js?"+Math.random();
+var s0 = document.getElementsByTagName('script')[0];
+s0.parentNode.insertBefore(sNew, s0);
